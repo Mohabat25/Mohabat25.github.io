66d3ae5fa75cc6ecd82185e70249d332efba0ca3
Mohabat25 <mohabatjitsingh@dcmail.ca>
2025-08-01 11:14:22 -0400

    update main.js
    alt text added for each images

--- Assignment-04/part02/main.js
+++ Assignment-04/part02/main.js
@@ -1,18 +1,19 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const altTexts = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Mountain landscape',
+  'pic3.jpg': 'City skyline at night',
+  'pic4.jpg': 'Forest trail',
+  'pic5.jpg': 'Snowy cabin in woods'
+};
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
-/* Wiring up the Darken/Lighten button */
