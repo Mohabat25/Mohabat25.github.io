327b08337066cdb8f2352c618c2a89484fa76882
Mohabat25 <mohabatjitsingh@dcmail.ca>
2025-08-01 10:29:41 -0400

    make some changes in part 1
    live server not opening deleted and created main.js again

--- Assignment-04/part01/main.js
+++ Assignment-04/part01/main.js
@@ -1,48 +1,49 @@
- //Name: Mohabatjit Singh
- //File: Part1.html 
- //Date: 31 July 2025 
- //JAVASCRIPT silly story generator.
+// Name: Mohabatjit Singh
+// File: main.js
+// Date: 1 August 2025
+// JavaScript for the silly story generator
+const customName = document.getElementById('customname');
+  const randomize = document.querySelector('.randomize');
+    const story = document.querySelector('.story');
 
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+    const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+      const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+       const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-const insertX = ['Bhoothnath', 'Mighty star', 'Jannat'];
-const insertY = ['wonderland', 'Chennai Express', 'Black Hole'];
-const insertZ = ['Brahmaa', 'Street King', 'Race'];
+      function randomValueFromArray(array) {
+       const random = Math.floor(Math.random() * array.length);
+        return array[random];
+      }
 
-function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
-}
+      randomize.addEventListener('click', result);
 
-randomize.addEventListener('click', result);
+      function result() {
+        let newStory =
+          'It was 94 fahrenheit outside, so :insertx: went for a walk. ' +
+          'When they got to :inserty:, they stared in horror for a few moments, then :insertz:. ' +
+          'Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-function result() {
-  let newStory =
-    'It was 94 fahrenheit outside, so :insertx: went for a walk. ' +
-    'When they got to :inserty:, they stared in horror for a few moments, then :insertz:. ' +
-    'Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+        const xItem = randomValueFromArray(insertX);
+        const yItem = randomValueFromArray(insertY);
+        const zItem = randomValueFromArray(insertZ);
 
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
+        newStory = newStory.replace(/:insertx:/g, xItem);
+        newStory = newStory.replace(':inserty:', yItem);
+        newStory = newStory.replace(':insertz:', zItem);
 
-  newStory = newStory.replace(/:insertx:/g, xItem);
-  newStory = newStory.replace(':inserty:', yItem);
-  newStory = newStory.replace(':insertz:', zItem);
+        if (customName.value !== '') {
+          const name = customName.value;
+          newStory = newStory.replace('Bob', name);
+        }
 
-  if (customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace('Bob', name);
-  }
+        if (document.getElementById('uk').checked) {
+          const weight = Math.round(300 * 0.0714286) + ' stone';
+          const temperature = Math.round((94 - 32) * (5 / 9)) + ' centigrade';
+          newStory = newStory.replace('94 fahrenheit', temperature);
+          newStory = newStory.replace('300 pounds', weight);
+        }
 
-  if (document.getElementById('uk').checked) {
-    const weight = Math.round(300 * 0.0714286) + ' stone';
-    const temperature = Math.round((94 - 32) * (5 / 9)) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit', temperature);
-    newStory = newStory.replace('300 pounds', weight);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
-}
+        story.textContent = newStory;
+        story.style.visibility = 'visible';
+      }
+    
