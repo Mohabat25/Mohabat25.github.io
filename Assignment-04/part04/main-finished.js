8d3f2870199e63c40ecaa60de5351475e92cd6df
Mohabat25 <mohabatjitsingh@dcmail.ca>
2025-08-01 14:27:31 -0400

    update discription
    added discription in part 4 main-finished.js

--- Assignment-04/part04/main-finished.js
+++ Assignment-04/part04/main-finished.js
@@ -1,31 +1,35 @@
-// set up canvas
+// Name: Mohabatjit Singh
+// File: main.js
+// Date: 1 August 2025
+// JavaScript for the bouncing balls animation
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// function to generate random RGB color value
+
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-class Shape {
-  constructor(x, y, velX, velY) {
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-  }
-}
-
-class Ball extends Shape {
-  constructor(x, y, velX, velY, color, size) {
-    super(x, y, velX, velY); 
     this.color = color;
     this.size = size;
-    this.exists = true; 
-}
+  }
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
